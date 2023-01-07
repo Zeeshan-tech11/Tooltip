f5b324303376da9d8cb0fc5d7a98ec8da3aa7da1
Zeeshan-one <zeeshan.ahmad.ext@fplabs.tech>
2023-01-08 00:44:39 +0530

    pushing to github

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import ToolTip from "./Compnent/ToolTip";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{display:"flex",justifyContent:"center",height:'100vh',alignItems:"center"}}>
+      <ToolTip position='left'>
+        <div>heyyy</div>
+        <div>hover over me to see tool tip</div>
+      </ToolTip>
     </div>
   );
 }
---
+++ src/Compnent/ToolTip.js
@@ -0,0 +1,77 @@
+import React, { useState } from 'react'
+import { VscTriangleDown, VscTriangleRight,VscTriangleLeft,VscTriangleUp} from 'react-icons/vsc';
+
+
+function ToolTip({children,position}) {
+  let[Position,setPosition]=useState(null)
+  console.log(position);
+  return (
+    <div onMouseEnter={()=>setPosition(position)} onMouseLeave={()=>setPosition(null)}>
+    {Position==='left'?
+      <>
+      <div style={{display:"inline-block"}}>
+      <div style={{display:"flex",flexDirection:"row",alignItems:"center"}}>
+      <ToolTipComponet/>
+      <VscTriangleRight size={'2rem'} style={{marginLeft:'-1rem'}}/>
+      <div>
+      {children}
+      </div>
+      </div>
+      </div>
+      </>
+      :Position==='right'?
+      <>
+      <div style={{display:"inline-block"}}>
+      <div style={{display:"flex",flexDirection:"row",alignItems:"center"}}>
+      <div>
+      {children}
+      </div>      <VscTriangleLeft size={'2rem'} style={{marginRight:'-1rem'}}/>
+      <ToolTipComponet/>
+      </div>
+      </div>
+      </>
+      :
+      Position==='bottom'?
+      <>
+      <div style={{display:"inline-block"}}>
+      <div style={{display:"flex",flexDirection:"column",alignItems:"center"}}>
+      <div>
+      {children}
+      </div>      <VscTriangleUp size={'2rem'} style={{marginBottom:'-1rem'}}/>
+      <ToolTipComponet/>
+      </div>
+      </div>
+      </>
+      :
+      Position==='top'?
+      <>
+      <div style={{display:"inline-block"}}>
+      <div style={{display:"flex",flexDirection:"column",alignItems:"center"}}>
+      <ToolTipComponet/>
+      <VscTriangleDown size={'2rem'} style={{marginTop:'-1rem'}}/>
+      <div>
+      {children}
+      </div>      </div>
+      </div>
+      </>
+      :
+      <div>{children}</div>
+      }
+      </div>
+  )
+}
+
+const ToolTipComponet=()=>{
+  return(
+    <>
+    <div style={{display:"inline-block",alignItems:"center",backgroundColor:"black",color:"white",textAlign:"center",padding:'.8rem',borderRadius:".5rem"}}>
+          <span>Thanks for hovering over me
+          </span>
+          <br/>
+          <span>I am a tool tip</span>
+    </div>
+    </>
+  )
+}
+
+export default ToolTip
